cad0c036b2c705f5eab88d11dceec7fa46d7f013
Joan <joandv.gil@gmail.com>
2023-09-23 19:07:07 -0300

    Cleanup

--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import ApolloLambdaWebsocketStack from "./lib/stack";
+import ApolloLambdaStack from "./lib/stack";
+
 const app = new cdk.App();
 
-const stack = new ApolloLambdaWebsocketStack(app, "ApolloLambdaWebsocketStack");
+const stack = new ApolloLambdaStack(app, "ApolloLambdaWebsocketStack");
 cdk.Tags.of(stack).add("project", "rick-and-morty-chat-api");
 cdk.Tags.of(stack).add("topic", "lambda-apollo-websockets-events");
 app.synth();
