baa3e8927754223a9ef8032981fdfefe2070af34
Joan <joandv.gil@gmail.com>
2023-09-23 17:05:48 -0300

    Add CORS Header

--- src/lambda/graphql-query-handler.ts
+++ src/lambda/graphql-query-handler.ts
@@ -4,9 +4,15 @@ import { ApolloServer } from "@apollo/server";
 import {
   startServerAndCreateLambdaHandler,
   handlers,
+  middleware,
 } from "@as-integrations/aws-lambda";
 
-import { Context } from "aws-lambda";
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyStructuredResultV2,
+  Context,
+} from "aws-lambda";
+import { RequestHandler } from "@as-integrations/aws-lambda/dist/request-handlers/_index";
 import typeDefs from "../graphql/schema";
 
 import {
@@ -36,9 +42,24 @@ const server = new ApolloServer({
   resolvers,
 });
 
+const corsMiddleware: middleware.MiddlewareFn<
+  RequestHandler<APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2>
+> = async () => (result) => {
+  // eslint-disable-next-line no-param-reassign
+  result.headers = {
+    ...result.headers,
+    "Access-Control-Allow-Origin": "*",
+    Vary: "Origin",
+  };
+  return Promise.resolve();
+};
+
 const handler = startServerAndCreateLambdaHandler(
   server,
   handlers.createAPIGatewayProxyEventV2RequestHandler(),
+  {
+    middleware: [corsMiddleware],
+  },
 );
 
 export default async function handleMessage(event: any) {
